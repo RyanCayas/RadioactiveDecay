d7c17a7f73303864035c93db257e4f172e5cec15
rogue8200 <rogue8200@gmail.com>
2017-05-06 22:06:19 +0800

    Made a bit of progress in GUI
    Alpha decay button can call a function but still dont know how to open a
    Python on press

--- GUI.py
+++ GUI.py
@@ -1,12 +1,30 @@
+import sys
+import tkFileDialog
+from tkFileDialog import askopenfilename
 import Tkinter
 from Tkinter import *
 
-main=Tk()
 
-firstbutton = Button (main, text = "first button", width=50, height=4)
-firstbutton.pack()
-secondbutton = Button (main, text = "second button", width=50, height=4)
-secondbutton.pack(side= BOTTOM)
-thirdbutton = Button (main, text = "third button", width=50, height=4)
-thirdbutton.pack(side = BOTTOM)
+
+main=Tk()
+main.title("Main Menu")
+
+def open1():
+
+    filename = askopenfilename(parent=main)
+    print(filename)
+    f = open(filename)
+    txt = f.read()
+    print txt
+    f.close()
+
+TitlePage = Label(main, text = "Main Menu", font = "Jokerman 50")
+TitlePage.pack()
+alpha = Button (main, text = "Alpha Decay", width=50, height=4, command = open1)
+alpha.pack()
+halflife = Button (main, text = "Half Life", width=50, height=4)
+halflife.pack(side= BOTTOM)
+guide = Button (main, text = "User's Guide", width=50, height=4)
+guide.pack(side = BOTTOM)
+
 main.mainloop()
